a16432f975b3ac69e4baa720f4fde60b1f9802dc
divya212109 <73348305+divya212109@users.noreply.github.com>
2021-05-22 14:01:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+objects = [];
+status = "";
+
+function preload(){
+  video = createCapture();
+}
+
+
+function setup() {
+  canvas = createCanvas(480, 380);
+  canvas.center();
+  video.hide();
+}
+
+function start()
+{
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Things";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+}
+
+function draw() {
+  image(video, 0, 0, 480, 380);
+  if(status != "")
+  {
+    objectDetector.detect(video, gotResult);
+    for (i = 0; i < objects.length; i++) {
+      document.getElementById("status").innerHTML = "Status : Objects Detected";
+      document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+
+      fill("#FF2454");
+      percent = floor(objects[i].confidence * 100);
+      text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+      noFill();
+      stroke("#FF9999");
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+  }
+}
